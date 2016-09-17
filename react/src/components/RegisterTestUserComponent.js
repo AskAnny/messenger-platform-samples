7f41592d40ef70a78e0a9711fa5b1415a6cd05b5
Maximilian <contact@mxlink.de>
2016-09-17 16:38:57 +0200

    started logix

--- react/src/components/RegisterTestUserComponent.js
+++ react/src/components/RegisterTestUserComponent.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
+let fullWidth = true;
+let primary = true;
+let inlineStyle = {
+  float: 'right'
+};
 class RegisterTestUserComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputText: ''
+    };
+  }
+
+  registerWithAnny(facebookUrl) {
+    // get id from url
+    // register id with app
+    console.log(facebookUrl);
+  }
+
   render() {
     return (
-      <div>
-        Hallo Maxi
+      <div className="centered">
+        <MuiThemeProvider>
+          <form>
+            <TextField
+              hintText="Link to your Facebook Account" fullWidth={fullWidth}
+              value={this.state.inputText}
+              onChange={(e) => this.setState({inputText: e.target.value})} />
+            <RaisedButton
+              label="Let's do it!" primary={primary} style={inlineStyle}
+              onClick={() => registerWithAnny(this.state.inputText)}
+            />
+          </form>
+        </MuiThemeProvider>
       </div>
     );
   }
