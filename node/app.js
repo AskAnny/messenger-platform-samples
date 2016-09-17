9c0ba77089a6207f6a6f6e757491899c1eb11162
Florian Dreschner <f.dreschner@beekeeper.io>
2016-09-17 10:09:51 +0200

    Use 5000 port again

--- node/app.js
+++ node/app.js
@@ -25,7 +25,7 @@ const
   GraphHandler = require('./handlers/graph');
 
 var app = express();
-app.set('port', process.env.PORT || 8443);
+app.set('port', process.env.PORT || 5000);
 app.set('view engine', 'ejs');
 app.use(bodyParser.json({ verify: verifyRequestSignature }));
 app.use(express.static('public'));
