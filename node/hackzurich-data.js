a522f58be20730892a8faab19c8b4cab73d419ec
Johannes Glöckle <johannes-gloeckle@t-online.de>
2016-09-17 03:52:37 +0200

    added hack zurich data. updated gitignore for vim

---
+++ node/hackzurich-data.js
@@ -0,0 +1,15 @@
+module.exports = {
+    hackZurichData : {
+      schedule: [[], ["07:00-09:00 // BREAKFAST", "09:00-12:00 // FREE MASSAGE"], ["07:00 - 09:00 // BREAKFAST", "09:00 // PROJECTS DEADLINE", "9:30 - 11:30 // JURY PROJECT EVALUATION", "12:30 // ANNOUNCEMENT OF FINALISTS", "12:00 - 13:00 // LUNCH", "13:30 - 16:00 // CLOSING CEREMONY & FINALIST PITCHES", "16:00 - 17:30 // APÉRO", "17:30 // THE END!"]],
+      workshops: ["AXA//“Insurance – As simple as a Game” aka “Clash of Claims”", "RAIFFEISEN, TI&M, ADUNO //The Conversational Future of Payment"],
+      techtalks: ["GOOGLE//Google's Developments in Cloud Machine Learning", "TAMEDIA//Create new and innovate applications by combining 20min, Doodle and Homegate", "Microsoft//Gamification", "SBB//It's all about data and location at Zurich main station!", "ADOBE//Introduction to Adobe Target & PhoneGap", "SAMSUNG//First Real Android Printing UX", "BOSCH//Connect with every Thing", "KAYAK//Designing Great Experiences at KAYAK", "SWISSCOM//Artificial Intelligence at Swisscom", "KTI//CTI Start-up - Swiss Start-up Acceleration"],
+      team: ["Jonathan Isenring", "Rasmus Rothe", "Setareh Gharibi", "Eirikur Agustsson"],
+      jury: ["Dr. Alexander Ilic", "Cédric Waldburger", "Clara Buchanan",  "Dr. Thomas Puschmann", "Pascal Mathis", "Christian Reiter", "Marc Gasser", "Georg Polzer", "Penny Schiffer", "Dr. Vital Meyer", "Claudia Bienentreu", "Ryan Ogle"],
+      email: "info@hackzurich.com",
+      facebook: "https://www.facebook.com/hackzurich",
+      instagram: "https://instagram.com/hackzurich",
+      twitter: "https://twitter.com/hackzurich",
+      linkedin: "https://www.linkedin.com/company/5256779"
+
+    }
+}
