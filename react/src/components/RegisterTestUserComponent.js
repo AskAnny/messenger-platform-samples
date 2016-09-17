4a9ced051293ca38d557f2ad3812f48b6a47157e
Maximilian <contact@mxlink.de>
2016-09-17 18:20:00 +0200

    wip

--- react/src/components/RegisterTestUserComponent.js
+++ react/src/components/RegisterTestUserComponent.js
@@ -3,6 +3,11 @@ import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
+// import Request from 'react-http-request';
+// const request = require('request');
+// import fetch from 'node-fetch';
+import 'whatwg-fetch';
+
 let fullWidth = true;
 let primary = true;
 let inlineStyle = {
@@ -16,10 +21,19 @@ class RegisterTestUserComponent extends React.Component {
     };
   }
 
-  registerWithAnny(facebookUrl) {
+  registerWithAnny(facebookName) {
     // get id from url
     // register id with app
-    console.log(facebookUrl);
+    // request(url: facebookUrl, function(error, response, body) {
+    //   if (error) {
+    //     console.log('error');
+    //   } else {
+    //     console.log(body);
+    //   }
+    // });
+    fetch('https://graph.facebook.com/' + facebookName).then(function(response) {
+      console.log(response);
+    })
   }
 
   render() {
@@ -33,7 +47,7 @@ class RegisterTestUserComponent extends React.Component {
               onChange={(e) => this.setState({inputText: e.target.value})} />
             <RaisedButton
               label="Let's do it!" primary={primary} style={inlineStyle}
-              onClick={() => registerWithAnny(this.state.inputText)}
+              onClick={() => this.registerWithAnny(this.state.inputText)}
             />
           </form>
         </MuiThemeProvider>
