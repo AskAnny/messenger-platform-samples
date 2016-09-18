3fcfd390570d60472403faedfc667125b0dd1bc0
Philip Kessler <philorlando92@yahoo.de>
2016-09-18 07:00:58 +0200

    Added popup

--- react/src/components/PageInputComponent.js
+++ react/src/components/PageInputComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 import FacebookLogin from 'react-facebook-login';
-
+import Modal from 'react-modal'
 import 'whatwg-fetch';
 
 
@@ -26,7 +26,9 @@ class PageInputComponent extends React.Component {
   }
 
   getPageToken(manageToken) {
-    fetch("https://www.graph.facebook.com/v2.7/me/accounts?access_token=" + manageToken).then(function(response) {
+    console.log("reached getPageToken");
+    fetch("https://graph.facebook.com/v2.7/me/accounts?access_token=" + manageToken).then(function(response) {
+      console.log(response);
       response.data.forEach(function(page) {
         if (page.id === this.state.inputText) {
           console.log("starting to register page: " + page.name);
@@ -41,6 +43,7 @@ class PageInputComponent extends React.Component {
   render() {
     return (
       <div>
+      <div>
         <form>
           <TextField
             hintText="URL to your Facebook Page" fullWidth={true}
@@ -51,15 +54,53 @@ class PageInputComponent extends React.Component {
             onClick={() => console.log(this.state.inputText)}
           />
         </form>
+
+        </div>
+        <div>
+        <Modal
+          isOpen={true}
+          style={customStyle}
+        >
+        <h1> Log dich ein! </h1>
         <FacebookLogin
           appId="729885603830764"
           autoLoad
           scope="manage_pages,pages_messaging"
-          callback={(response) => this.getPageToken(response.access_token)}
+          callback={(response) => this.getPageToken(response.accessToken)}
+
           size="small"
           icon="fa-facebook"/>
+        </Modal>
+        </div>
         </div>
     );
+  }
+}
+
+const customStyle = {
+  color : 'black',
+  overlay : {
+    position          : 'fixed',
+    top               : 0,
+    left              : 0,
+    right             : 0,
+    bottom            : 0,
+    backgroundColor   : 'rgba(255, 255, 255, 0.75)'
+  },
+  content : {
+    position                   : 'absolute',
+    top                        : '40px',
+    left                       : '40px',
+    right                      : '40px',
+    bottom                     : '40px',
+    border                     : '1px solid #ccc',
+    background                 : '#fff',
+    overflow                   : 'auto',
+    WebkitOverflowScrolling    : 'touch',
+    borderRadius               : '4px',
+    outline                    : 'none',
+    padding                    : '20px'
+
   }
 }
 
